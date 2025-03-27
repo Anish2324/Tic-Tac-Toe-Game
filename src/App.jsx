54828bc9ce430f34806572666c1925d69c9c30f6
Anish <anish18ise@gmail.com>
2025-03-27 23:59:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Tic_Tac_Toe from './components/Tic-tac-toe';
+
+function App() {
+  return (
+    <div>
+        <Tic_Tac_Toe/>
+    </div>  
+  )
+}
+
+export default App;
